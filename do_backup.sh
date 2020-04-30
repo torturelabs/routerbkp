16db207e99c82de5325363a4060f4f539a5dc3f5
Dmitry Ilyin <dima@doty.ru>
2020-04-30 16:25:51 +0300

    Rework do_backup.sh

--- do_backup.sh
+++ do_backup.sh
@@ -1,31 +1,38 @@
 #!/usr/bin/env bash
 
-set -x
 set -e
 
 source .env
 
-# Return the canonicalized path (works on OS-X like 'readlink -f' on Linux); . is $PWD
-function realpath {
-    [ "." = "${1}" ] && n=${PWD} || n=${1}; while nn=$( readlink -n "$n" ); do n=$nn; done; echo "$n"
-}
-
 COMPACT=$(/bin/date +%Y%m%d_%H%M)
-SSHKEY="${MYPATH}/.ssh/id_rsa"
+
+eval $(ssh-agent -s)
+echo "$PRIVATE_KEY" | ssh-add -
+trap "ssh-agent -k" EXIT
 
 cd ${CONFDIR}
 
 while read -r line; do
+  ROUTER="$(echo $line | cut -d':' -f1)"
+  SSHPORT="$(echo "$line:" | cut -d':' -f2)"
+  SSHPORT=${SSHPORT:-22}
 
-  ROUTER=$line
-  echo Processing ${ROUTER}
+  NEWROUTER="${USERONROUTER}@${ROUTER}"
+  SSHCREDS="-p ${SSHPORT} ${NEWROUTER}"
+  SCPCREDS="-P ${SSHPORT}"
+  echo "Processing $NEWROUTER"
 
-  /usr/bin/ssh -n -i ${SSHKEY} -oStrictHostKeyChecking=no -oConnectTimeout=10 $USERONROUTER@$ROUTER "/export file=export_${COMPACT}" &&
-    /usr/bin/scp -i ${SSHKEY} -q $USERONROUTER@$ROUTER:/export_${COMPACT}.rsc ${CONFDIR}/${ROUTER} &&
-    /usr/bin/ssh -n -i ${SSHKEY} $USERONROUTER@$ROUTER "/file remove export_${COMPACT}.rsc" &&
-    sed -i -e "1d" ${CONFDIR}/${ROUTER}
+  /usr/bin/ssh -n -oStrictHostKeyChecking=no -oConnectTimeout=10 \
+    ${SSHCREDS} "/export file=export_${COMPACT}" &&
+    /usr/bin/scp ${SCPCREDS} -q ${NEWROUTER}:/export_${COMPACT}.rsc \
+      ${CONFDIR}/${ROUTER} &&
+    /usr/bin/ssh -n ${SSHCREDS} "/file remove export_${COMPACT}.rsc" &&
+    sed -i.bak -e "1d" ${CONFDIR}/${ROUTER}
 
 done <"${ROUTERS}"
 
+rm "${CONFDIR}"/*.bak
+
 git add ${CONFDIR}
 git commit -m "Autocommit"
+echo "SUCCESS"
